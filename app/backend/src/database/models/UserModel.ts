0f6d6ece78d4fe5e3d709df172be30ae378972ff
Scarlat Pereira <engcivil.scarlat@gmail.com>
2023-03-29 18:02:58 -0300

    feat: create userModel

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,41 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  declare id: number;
+  declare userName: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+Users.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  userName: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default Users;
