843199954fbfbb2ce388dd879cb1d9a352e7bef2
Scarlat Pereira <engcivil.scarlat@gmail.com>
2023-03-31 09:45:54 -0300

    refactor: removing comments from files TeamService and TeamController

--- app/backend/src/controllers/TeamController.ts
+++ app/backend/src/controllers/TeamController.ts
@@ -1,19 +1,6 @@
 import { Request, Response } from 'express';
 import TeamService from '../services/TeamService';
 
-// const getAll = async (_req: Request, res: Response) => {
-//   const teams = await TeamService.getAll();
-//   return res.status(200).json(teams);
-// };
-
-// const getAllTeamById = async (req: Request, res: Response) => {
-//   const { id } = req.params;
-//   const team = await TeamService.getAllTeamById(Number(id));
-//   return res.status(200).json(team);
-// };
-
-// export default { getAll, getAllTeamById };
-
 export default class TeamController {
   _service: TeamService;
 
--- app/backend/src/services/TeamService.ts
+++ app/backend/src/services/TeamService.ts
@@ -1,21 +1,6 @@
 import { ModelStatic } from 'sequelize';
 import Teams from '../database/models/indexModel';
 
-// async function getAll(): Promise<Teams[]> {
-//   const teams = await TeamModel.findAll();
-//   return teams;
-// }
-
-// async function getAllTeamById(id: number) {
-//   const team = await TeamModel.findByPk(id);
-//   return team;
-// }
-
-// export default {
-//   getAll,
-//   getAllTeamById,
-// };
-
 export default class TeamService {
   _model: ModelStatic<Teams>;
 
